458bbcfb949ad6951e0aecedb59252df7dc9cfaf
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2019-12-12 17:17:02 +0200

    Created basic editor grid snapping tool, added world space UI to reflect cube position in grid.

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    TextMeshProUGUI worldSpaceText;
+    Vector3 snapPos;
+
+    private void Start()
+    {
+        worldSpaceText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+        worldSpaceText.text = $"{snapPos.x / gridSize}.{snapPos.z / gridSize}";
+    }
+}
